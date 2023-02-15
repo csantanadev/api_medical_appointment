d3e63706e3710d6b6fb72ff1671ae8527bc955d9
Carlos Santana <csantana@informa.com.br>
2023-02-15 17:21:45 -0300

    zod

--- src/modules/doctor/useCases/create-doctor/create-doctor.controller.ts
+++ src/modules/doctor/useCases/create-doctor/create-doctor.controller.ts
@@ -1,6 +1,11 @@
+import { ValidationSchemaError } from './../../../../errors/validation-schema.error';
+import { validatorSchema } from './../../../../infra/shared/validator/zod';
 import { Request, Response } from "express";
 import { CreateDoctorUseCase } from './create-doctor.usecase';
 import { StatusCodes } from "http-status-codes";
+
+import { z } from 'zod';
+
 
 export class CreateDoctorController {
 
@@ -9,14 +14,35 @@ export class CreateDoctorController {
     handle = async (request: Request, response: Response) => {
 
         try {
-            const data = request.body;
+            const { body } = request;
 
-            const doctor = await this.createDoctorUseCase.execute(data);
+            const doctorSchema = z.object({
+                username: z.string(),
+                name: z.string(),
+                email: z.string().email({
+                    message: 'You need to insert a valid email'
+                }),
+                password: z.string(),
+                crm: z.string().length(6, {
+                    message: 'CRM must contain 6 characteres'
+                }),
+                specialityId: z.string().uuid({
+                    message: 'You need to insert a valid specialityId'
+                })
+            });
+
+            // validação de schema
+            validatorSchema(doctorSchema, body);
+
+            const doctor = await this.createDoctorUseCase.execute(body);
 
             return response.status(StatusCodes.CREATED).json(doctor);
         }
-        catch (err: any) {
-            return response.status(err.statusCode).json({ error: err.message })
+        catch (error: any) {
+            if (error instanceof ValidationSchemaError) {
+                return response.status(error.statusCode).json(error.errors);
+            }
+            return response.status(error.statusCode).json(error.message);
         }
     }
 
